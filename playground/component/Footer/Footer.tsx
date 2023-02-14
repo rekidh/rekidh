05cb011174c83d02471d02a2f1f73ebbe4479852
rekidh <89791981+rekidh@users.noreply.github.com>
2023-02-14 22:42:56 +0700

    footer need to recheck

---
+++ playground/component/Footer/Footer.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import style from './footer.module.css'
+
+export default function Footer() {
+  return (
+    <div className={style.c}>
+      <div className={style.rapper}>
+            <ul>
+               <h3>Portfolio</h3>
+               <li><a href=""></a>Dribbble</li>
+               <li><a href=""></a>Behance</li>
+               <li><a href=""></a>Codepen</li>
+               <li><a href=""></a>Github</li>
+            </ul>
+            <ul>
+               <h3>Portfolio</h3>
+               <li><a href=""></a>Dribbble</li>
+               <li><a href=""></a>Behance</li>
+               <li><a href=""></a>Codepen</li>
+               <li><a href=""></a>Github</li>
+            </ul>
+            <ul>
+               <h3>Portfolio</h3>
+               <li><a href=""></a>Dribbble</li>
+               <li><a href=""></a>Behance</li>
+               <li><a href=""></a>Codepen</li>
+               <li><a href=""></a>Github</li>
+            </ul>
+      </div>
+    </div>
+  )
+}
--- playground/pages/index.tsx
+++ playground/pages/index.tsx
@@ -4,6 +4,7 @@ import Section1 from '@/component/Section1/Section1';
 import Section2 from '@/component/Section2/Section2';
 import Section3 from '@/component/Section3/Section3';
 import Section4 from '@/component/Section4/Section4';
+import Footer from '@/component/Footer/Footer';
 
 // const inter = Inter({ subsets: ['latin'] })
 
@@ -17,6 +18,7 @@ export default function Home() {
       <Section2/>
       <Section3/>
       <Section4/>
+      <Footer/>
     </>
   )
 }
