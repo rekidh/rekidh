a7d9ff014ad198695893a2b745cabe9a4e3aa08a
rekidh <89791981+rekidh@users.noreply.github.com>
2023-02-22 18:47:20 +0700

    -

--- playground/component/Section2/Section2.tsx
+++ playground/component/Section2/Section2.tsx
@@ -15,27 +15,34 @@ type DataType = {
 
 function Section2() {
   const [data, setData] = useState<DataType[]>([] as DataType[])
+  const [slider,setSlider] = useState<DataType>()
   const getData = async () => {
     const host = window.location.href
     await axios.get(`${host}api/working`).then(res => setData(res.data))
   }
   useEffect(()  => {
+    console.log('data sedang di panggil')
     getData()
   },[])
+  useEffect(()  => {
+    console.log('data sudah di dapatakan')
+    setSlider(data[0])
+  },[data])
 
   return (
     <div className={style.section_2}  id='work'>
         <h1>Working Experience</h1>
             {
-              data.map((item) => (
-                <div className={style.card_section_2}>
+              // data.map((item) =>
+              slider? (
+                <div className={style.card_section_2} >
                 <div className={style.card_title}>
-                  <h1 >{item.jobTitle.toString()}</h1>
-                  <p >{item.starDate.toString()}</p>
+                  <h1 >{slider.jobTitle.toString()}</h1>
+                  <p >{slider.starDate.toString()}</p>
                 </div>
                 <ul className={style.card_list}>
                   {
-                    item.point.map((item2) => 
+                    slider.point.map((item2) => 
                       (
                         <li>
                           <Image src={cheklist} width={22} height={22} alt='reki'/>
@@ -47,15 +54,17 @@ function Section2() {
                 </ul>
                 <div className={style.card_footer}>
                   <Button model='' size={{x:140,y:50}}>See Detail</Button>
-                  <h1 >{item.perusahaan}</h1>
+                  <h1 >{slider.perusahaan}</h1>
                 </div>
               </div>
-                ))
+              ):(<div>loading...</div>)
+                // ))
             }
         <div className={style.dot}>
-          <span></span>
-          <span></span>
-          <span></span>
+          {data?data.map((value,index)=>(
+            <span onClick={()=>setSlider(data[index])}></span>
+          )):''}
+
         </div>
       </div>
   )
