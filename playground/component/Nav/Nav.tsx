bd83db4457eb4d76a3bf89392ec041f507397788
rekidh <89791981+rekidh@users.noreply.github.com>
2023-02-27 14:52:31 +0700

    burger menu

--- playground/component/Nav/Nav.tsx
+++ playground/component/Nav/Nav.tsx
@@ -3,14 +3,17 @@ import Link from "next/link"
 import style from "./Nav.module.css"
 import Button from "../atomic/Button/Button"
 import { useState } from "react"
-export default function Nav(){
+interface Nav{
+   dataStatus:string;
+}
+export default function Nav(prop:Nav){
 
-   const[status, SetStatus]=useState<boolean>()
+   const[status, SetStatus]=useState<boolean>(false)
 
    return(
       <nav className={style.nav}>
          <div className={style.logo}>Rekidh</div>
-         <div className={style.nav_menu} status={`${status?'active':'inactive'}`} >
+            <div className={`${style.nav_menu} ${status?style.status:''}`} >
             <ul>
                <li><Link href='#about'>About</Link> </li>
                <li><Link href='#work'>Working Experience</Link> </li>
@@ -20,7 +23,7 @@ export default function Nav(){
          <div className={style.container_btn}>
             <Button model="primary" size={{x:140,y:50}}> Let's Talk</Button>
          </div>
-         <div className={style.hamburger_menu} status={`${status?'active':'inactive'}`}  onClick={(e)=>SetStatus(!status)}> 
+         <div className={`${style.hamburger_menu} ${status?style.active:''}`}  onClick={(e)=>SetStatus(!status)}> 
             <span></span>
             <span></span>
             <span></span>
