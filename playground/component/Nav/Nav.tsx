28cf1c9841c07ec26e901239402bc968f71bb92d
rekidh <89791981+rekidh@users.noreply.github.com>
2023-02-27 14:56:27 +0700

    burger menu

--- playground/component/Nav/Nav.tsx
+++ playground/component/Nav/Nav.tsx
@@ -3,10 +3,8 @@ import Link from "next/link"
 import style from "./Nav.module.css"
 import Button from "../atomic/Button/Button"
 import { useState } from "react"
-interface Nav{
-   dataStatus:string;
-}
-export default function Nav(prop:Nav){
+
+export default function Nav(){
 
    const[status, SetStatus]=useState<boolean>(false)
 
